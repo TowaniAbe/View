6c8ae7c213d1825098e6728c5a9686d841ce87b5
TowaniAbe <banhugh.7@gmail.com>
2020-09-24 05:48:56 +0000

    view 課題

---
+++ edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PHP日記</title>
+    </head>
+    <body>
+        <h1>PHPのLaravel勉強の歩み</h1>
+        <h3>最後まで勉強頑張ります！</h3>
+    </body>
+</html>
