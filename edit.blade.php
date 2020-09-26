bf77fd9d12d1ae61511855c51f76fc9e656b8f9b
TowaniAbe <banhugh.7@gmail.com>
2020-09-26 03:09:56 +0000

    Laravel 12 課題

--- edit.blade.php
+++ edit.blade.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/prolile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>PHP日記</title>
-    </head>
-    <body>
-        <h1>PHPのLaravel勉強の歩み</h1>
-        <h3>最後まで勉強頑張ります！</h3>
-    </body>
-</html>
+
+{{-- prolile.blade.phpの@yield('title')に'PHP勉強日記'を埋め込む --}}
+@section('title', 'PHP勉強日記')
+
+{{-- prolile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>PHPのLaravel勉強の記録</h2>
+                <h3>覚えるまで諦めなければものになる！</h3>
+            </div>
+        </div>
+    </div>
+@endsection
